982f8cbdfa90672a1934d3dfa9767b1c1f4adf0b
Daigo Fujiwara <daigo@daigofujiwara.com>
2017-03-27 12:17:26 -0400

    Indentation

--- js/app.js
+++ js/app.js
@@ -7,19 +7,21 @@ $(document).ready(function()
     } );
 
 
-    new Chartist.Line('.ct-chart', {
-    labels: ['2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016'],
-    series: [
-      [1651, 1540, 1526, 1635, 1544, 1667, 1479, 1383, 1343, 1200, 1025]
-    ]
-    }, {
-      fullWidth: true,
-      chartPadding: {
-        right: 40
-      },
-      low: 0,
-      showArea: true
-    });
+    new Chartist.Line('.ct-chart', 
+      {
+        labels: ['2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016'],
+        series: [
+          [1651, 1540, 1526, 1635, 1544, 1667, 1479, 1383, 1343, 1200, 1025]
+        ]
+      }, {
+        fullWidth: true,
+        chartPadding: {
+          right: 40
+        },
+        low: 0,
+        showArea: true
+      }
+    );
 
   }
 ).foundation();
